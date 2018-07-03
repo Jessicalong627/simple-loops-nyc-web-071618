57247fd0d7ee4725cf2327142703fb9fcc19b1e9
Jessica Long <jessicalong627@gmail.com>
2018-07-03 02:07:05 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -16,6 +16,13 @@ def loop_iterator(number_of_times)
   end
 end
 
+
+  z = 0
+  loop do
+    z+=1
+    puts "Hello Ruby-007!"
+    break if z == number_of_times
+    
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
